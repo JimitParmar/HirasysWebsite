7450007a278983212254d5e11ba38b616b07e693
JimitParmar <Jimit.parmar@somaiya.edu>
2024-06-29 18:19:04 +0530

    blog page updated

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,8 @@
-import { title } from "@/components/primitives";
-import { BlogCard, MainBlogCard } from "@/components/card";
+import { subtitle, title } from "@/components/primitives";
+import { BlogCard, CustomImage, MainBlogCard, DualImage} from "@/components/card";
+import Image from "next/image";
+import { button as buttonStyles } from "@nextui-org/theme";
+import { px } from "framer-motion";
 
 
 export default function BlogPage() {
@@ -12,10 +15,9 @@ export default function BlogPage() {
             <MainBlogCard
               image="/sbl.jpeg"
               title="The Emergence of Skills-Based Learning is Vital to Teams Success"
-              readingTime="20 minutes"
               author="Jimit Parmar"
               category="Articles"
-              linkto="google.com" />
+              linkto="https://peoplemanagingpeople.com/articles/skills-based-learning/" />
           </div>
           <div className="grid gap-y-6">
             <BlogCard
@@ -46,11 +48,48 @@ export default function BlogPage() {
         </div>
       </div>
     </section>
-    <section className="w-screen bg-black ">
-        <div className="w-20 h-200 text-red-600">
-          hello mister
+    <section className="w-screen bg-black pb-16 md:pb-32">
+          <div className="grid grid-cols-1 md:grid-cols-2 px-6 gap-8 md:px-32 pt-8 md:pt-32 text-white">
+            <div className="flex justify-center md:scale-100 ">
+            <CustomImage 
+            image="trends.jpeg"/>
+            </div>
+          <div className="flex flex-col gap-2">
+            <p className="pt-6 md:pt-12 pb-3 md:pb-6 text-md font-semibold uppercase">Strategy & Leadership</p>
+            <h1 className="text-2xl md:text-5xl font-semibold md:leading-[3.5rem] pb-4 md:pb-8 hover:underline underline-offset-8 ">HR Predictions: The 2024 Workplace Trends on the Minds of HR Leaders Everywhere</h1>
+            <p className="pb-6 md:pb-8">What do HR leaders and experts across industries think about AI, the talent marketplace and key initiatives around DEI and…</p>
+            <a className={buttonStyles({
+              variant:"bordered",
+              className:"max-w-40 p-6 text-white bg-blue-800 hover:bg-blue-600 hover:scale-105 transition-all ease-in-out delay-75 text-lg border-0",
+              radius: "full",
+              size: "md",
+            })}href="">Read</a>
+          </div>
         </div>
-      </section></>
+      </section>
+      <section>
+      <p className="py-8 pl-5 md:pb-8 md:pt-12 md:pl-40 text-xl font-medium">Latest Stories</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 md:px-40 mb-20 gap-8 md:gap-8">
+          <div className="flex flex-col">
+          <DualImage
+          image="recruitment.jpeg"/>
+          <h1 className="px-5 pt-2 text-lg md:pl-12 md:pt-6 md:text-2xl font-semibold hover:underline underline-offset-4 hover:text-blue-900 dark:hover:text-blue-700">Top 10 Recruitment Softwares of 2024</h1>
+          <p className="px-5 pt-2 md:px-12  md:pt-2 text-sm">Top applicant tracking systems to 
+            shortlist and manage candidates and uncover the best talent fit for each role.</p>
+            <h2 className="px-5 py-2 md:px-12  md:pt-2 font-semibold text-md">By Sanchit Warang</h2>
+          </div>
+          
+          <div>
+          <DualImage
+          image="hr.jpeg"/>
+          <h1 className="px-5 pt-2 md:pl-12 md:pt-6 text-lg md:text-2xl font-semibold hover:underline underline-offset-4 hover:text-blue-900 dark:hover:text-blue-700">41 Key HR Policies & Best Practices</h1>
+          <p className="px-5 pt-2 md:px-12  md:pt-2 text-sm">HR policies empower and protect employees 
+            and minimize business risks. Here are the best practices for developing your own.</p>
+            <h2 className="px-5 py-2 md:px-12  md:pt-2 font-semibold text-md">By Atharva Wagh</h2>
+          </div>
+        </div>
+        </section>
+        </>
 
   );
 }
--- components/card.tsx
+++ components/card.tsx
@@ -107,7 +107,7 @@ export function BlogCard({ image, title, category, author, linkto }:
     <div className="flex items-center ">
       <div className="flex flex-col ">
         <p className="text-md md:text-xs text-blue-700 mt-1 dark:text-blue-500 hover:underline underline-offset-4 dark:hover:text-blue-700 hover:text-blue-800 uppercase font-semibold mb-2 tracking-widest">{category}</p>
-        <h4 className=""><Link className="w-full text-black hover:underline underline-offset-4 dark:text-white dark:hover:text-blue-500 hover:text-blue-700 font-medium md:font-semibold text-xl md:text-[1.55rem] leading-8" href={linkto}>{title}</Link></h4>
+        <h4 className=""><Link className="w-full text-black hover:underline underline-offset-4 dark:text-white dark:hover:text-blue-500 hover:text-blue-700 font-semibold md:font-semibold text-xl md:text-[1.55rem] leading-8" href={linkto}>{title}</Link></h4>
         <p className="md:absolute md:mt-[140px] font-medium md:font-normal text-black dark:text-white text-lg md:text-sm my-4">By {author}</p>
       </div>
     </div>
@@ -118,12 +118,11 @@ export function BlogCard({ image, title, category, author, linkto }:
 }
 
 
-export function MainBlogCard({ image, title, author, readingTime, category, linkto }:
+export function MainBlogCard({ image, title, author, category, linkto }:
     {
         image:string,
         title:string,
         author:string,
-        readingTime:string,
         category: string,
         linkto:string
     }
@@ -140,7 +139,7 @@ export function MainBlogCard({ image, title, author, readingTime, category, link
             <div className="flex mt-4 md:mt-8 md:px-2">
                 <div className="flex flex-col pl-1">
                     <p className="text-md text-blue-700 dark:text-blue-500 hover:underline underline-offset-4 hover:text-blue-900 dark:hover:text-blue-700 uppercase tracking-widest font-semibold mb-3">{category}</p>
-                    <h4 className=""><Link className="text-black dark:text-white hover:underline underline-offset-4 hover:text-blue-700 dark:hover:text-blue-500 font-bold text-xl md:text-3xl"href={linkto}>{title}</Link></h4>
+                    <h4 className=""><a className="text-black dark:text-white hover:underline underline-offset-4 hover:text-blue-700 dark:hover:text-blue-500 font-bold text-2xl md:text-3xl" href={linkto}>{title}</a></h4>
                     <p className="text-black dark:text-white font-medium text-lg my-4">By {author}</p>
                 </div>
             </div></>
@@ -148,4 +147,42 @@ export function MainBlogCard({ image, title, author, readingTime, category, link
     );
 }
 
+export function CustomImage({ image}:
+    {
+        image:string,
+    }
+ ) {
+    return(
+            <>
+            <div className="flex justify-center">
+            <Image
 
+                alt="Relaxing app background"
+                className="h-[300px] w-[300px] md:h-[560px] md:w-[560px] hover:scale-105 rounded object-cover"
+                src={image} />
+            </div>
+            </>
+            
+    );
+}
+
+
+
+export function DualImage({ image}:
+    {
+        image:string,
+    }
+ ) {
+    return(
+            <>
+            <div className="flex justify-center">
+            <Image
+
+                alt="Relaxing app background"
+                className="h-[170px] w-[350px] md:h-[275px] md:w-[500px] hover:scale-105 rounded object-cover"
+                src={image} />
+            </div>
+            </>
+            
+    );
+}
